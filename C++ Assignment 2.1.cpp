b21809667ede1b37438066f8175772cd34add545
Reimakoo <reimaku@metropolia.fi>
2023-11-05 20:20:44 +0200

    Add project files.

---
+++ C++ Assignment 2.1.cpp
@@ -0,0 +1,17 @@
+// C++ Assignments.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "int: " << sizeof(int) << " tavua" << std::endl;
+    cout << "unsigned int: " << sizeof(unsigned int) << " tavua" << std::endl;
+    cout << "float: " << sizeof(float) << " tavua" << std::endl;
+    cout << "double: " << sizeof(double) << " tavua" << std::endl;
+    cout << "char: " << sizeof(char) << " tavua" << std::endl;
+    cout << "bool: " << sizeof(bool) << " tavua" << std::endl;
+
+    return 0;
+}
